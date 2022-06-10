1d554c10bb80dcd9affd0abaf8d6594c2170a48e
Aaron Enyetu <aaronenyetu@gmail.com>
2022-06-10 10:48:48 -0700

    Animal images and buttons working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import { useState } from 'react'; 
 import './App.css';
+import OpenClosedSign from './OpenClosedSign';
 
 function App() {
+// tracking the state with useState hooks:
+//LightSign should be a string that starts out as 'green'
+  const [LightSign, setLightSign] = useState('green');
+//gorillaSize should be a number that starts out as 10
+  const [gorillaSize, setGorillaSize] = useState(10);
+//lionSize should be a number that starts out as 10
+  const [lionSize, setLionSize] = useState(10);
+//animal parade should be an array of strings that starts out as [giraffe, antelope, buffalo, elephant, leopard, rhino, lion, gorilla, ostrich ]
+  const [animals, setAnimals] = useState (['elephant', 'buffalo', 'rhino', 'leopard', 'ostrich']);
+
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="fight">
+        <div className='wild'>
+          {/* the width of the gorilla should be ten times whatever the gorilla size is in state */}
+          <img src="gorilla.png" width={10 * gorillaSize} />
+          <div className='buttons'>
+            {/* when you click this button, the gorilla's size in state should go up by one */}
+            <button onClick={() => setGorillaSize(gorillaSize + 1)}>Angry gorilla is charging up</button>
+            {/* when you click this button, the gorilla's size in state should go down by one */}
+            <button onClick={() => setGorillaSize(gorillaSize - 1)}>The gorilla is getting defeated</button>
+          </div>
+        </div>
+        <div className='wild'>
+          {/* the width of the lion should be ten times whatever the gorilla size is in state */}
+          <img src="lion.jpg" width={10 * lionSize} />
+          <div className="buttons">
+            {/* when you click this button, the lion's size in state should go up by one */}
+            <button onClick={() => setLionSize(lionSize + 1)}>Angry lion is charging up</button>
+            {/* when you click this button, the gorilla's size in state should go down by one */}
+            <button onClick={() => setLionSize(lionSize - 1)}>The lion is getting defeated</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
